ac3e1d4c82d20cb1e64a8c7d31eb7ffc16133c48
tawakuliKH <tawakuli456@gmail.com>
2023-04-27 00:34:48 +0430

    Previouse

--- js/books.js
+++ js/books.js
@@ -45,28 +45,4 @@ class BookList {
 }
 
 const bookList = new BookList();
-
-const navList = document.querySelector('.menuList');
-const navAdd = document.querySelector('.menuAdd');
-const navContact = document.querySelector('.menuContact');
-const addBooks = document.querySelector('.add-book');
-const listBooks = document.querySelector('.list');
-const contactInfo = document.querySelector('.contact');
-
-navAdd.addEventListener('click', () => {
-  addBooks.style.display = 'flex';
-  listBooks.style.display = 'none';
-  contactInfo.style.display = 'none';
-});
-
-navList.addEventListener('click', () => {
-  window.location.reload();
-});
-
-navContact.addEventListener('click', () => {
-  contactInfo.style.display = 'flex';
-  listBooks.style.display = 'none';
-  addBooks.style.display = 'none';
-});
-
 bookList.init();
